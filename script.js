5b1c2df00d33eff8d72f7566792c4a9e7febe408
Eugene Mirotin <emirotin@gmail.com>
2015-06-21 17:54:40 +0300

    skills list

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log(DB.skills);
+(function () {
+  var skills = DB.skills, developers = DB.developers;
+
+  var app = new Ractive({
+    el: '#root',
+    template: '#tpl-app',
+    data: {
+      skills: _.keys(DB.skills)
+    }
+  });
+
+}());
