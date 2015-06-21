b9f00e93bc2490775271f23623dbade81e4b78d1
Eugene Mirotin <emirotin@gmail.com>
2015-06-21 18:05:53 +0300

    skills filter

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 (function () {
   var skills = DB.skills, developers = DB.developers;
+  var skillNames = _.keys(DB.skills);
 
   var app = new Ractive({
     el: '#root',
     template: '#tpl-app',
     data: {
-      skills: _.keys(DB.skills)
+      skillFilter: null,
+      skills: function() {
+        var skillFilter = this.get('skillFilter');
+        if (!skillFilter) {
+          return skillNames;
+        }
+        skillFilter = new RegExp(_.escapeRegExp(skillFilter), 'i')
+        return _.filter(skillNames, function(skill) {
+          return skill.match(skillFilter);
+        });
+      }
     }
   });
 
